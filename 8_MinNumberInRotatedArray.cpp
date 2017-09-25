8c6f662d13abd5810c3bb5dc5456b5d5c8134ff7
Wang Junming <wangjmx@gmail.com>
2017-09-25 16:51:43 +0800

    support special situation: the sorted array itself and fix the bug when items median, low and high are equal

--- 8_MinNumberInRotatedArray.cpp
+++ 8_MinNumberInRotatedArray.cpp
@@ -3,22 +3,44 @@
 
 using namespace std;
 
+int MinInorder(int* nums, int low, int high)
+{
+    int result = nums[low];
+    for (int i = low+1; i <= high; i++)
+    {
+        if (nums[i] < result)
+            result = nums[i];
+    }
+    return result;
+}
+
 int Min(int* nums, int length)
 {
     if (nums == NULL || length <= 0)
         throw new runtime_error("argument invalid");
 
-    int low = 0, high = length;
-    while (low < high)
+    int low = 0, high = length - 1;
+    int m = 0;
+    while (nums[low] >= nums[high])
     {
+        if (high - low == 1)
+        {
+            m = high;
+            break;
+        }
+
         int m = (low + high) / 2;
-        if (m > 0 && nums[m] < nums[m-1])
-            return nums[m];
-        else if (nums[m] >=  nums[0])
-            low = m + 1;
+
+        if (nums[low] == nums[m] && nums[high] == nums[m])
+            return MinInorder(nums, low, high);
+
+        else if (nums[m] >= nums[low])
+            low = m;
         else
             high = m;
     }
+
+    return nums[m];
 }
 
 int main()
@@ -31,5 +53,15 @@ int main()
 
     int nums3[] = {5,1,2,3,4};
     cout <<Min(nums3, 5) << endl;
+
+    int nums4[] = {1,2,3,4,5};
+    cout << Min(nums4, 5) << endl;
+
+    int nums5[] = {1,0,1,1,1};
+    cout << Min(nums5, 5) << endl;
+
+    int nums6[] = {1,1,1,0,1};
+    cout << Min(nums5, 5) << endl;
+
     return 0;
 }
