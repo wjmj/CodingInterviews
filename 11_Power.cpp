982f5a44352b4247d53a6818e0d805c978114689
Wang Junming <wangjmx@gmail.com>
2017-09-26 17:20:22 +0800

    interger square of the double value

---
+++ 11_Power.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+double Power(double base, int exponent)
+{
+    if (base == 0)
+        return 0;
+
+    if (exponent == 0)
+        return 1;
+
+    if (exponent > 0)
+    {
+        
+        if (exponent % 2 == 0)
+            return Power(base, exponent/2) * Power(base, exponent/2);
+
+        else
+            return base * Power(base, exponent/2) * Power(base, exponent/2);
+    }
+
+    else
+    {
+        if (exponent % 2 == 0)
+            return 1 / (Power(base, -exponent/2) * Power(base, -exponent/2));
+        else
+            return 1 / (base * Power(base, -exponent/2) * Power(base, -exponent/2));
+    }
+}
+
+int main()
+{
+    cout << Power(0, 1) << endl;
+    cout << Power(1, 2) << endl;
+    cout << Power(9, 3) << endl;
+  
+    cout << Power(0, -1) << endl;
+    cout << Power(1, -2) << endl;
+    cout << Power(9, -3) << endl;
+
+    cout << Power(-1, 2) << endl;
+    cout << Power(-9, -3) << endl;
+
+    return 0;
+}
