5ad5b1bc15c94c50a534021f3a705a3f4fdee9c7
Wang Junming <wangjmx@gmail.com>
2017-10-14 19:39:58 +0800

    reverse a list

---
+++ 16_ReverseList.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int m_nKey;
+    ListNode* m_pNext;
+}; 
+
+void ReverseList(ListNode** pHead)
+{
+    if (pHead == NULL || *pHead == NULL)
+        return;
+    ListNode* newHead = NULL;
+    ListNode* cursor = *pHead;
+    while (cursor != NULL)
+    {
+        *pHead = (*pHead)->m_pNext;
+        cursor->m_pNext = newHead;
+        newHead = cursor;
+        cursor = *pHead;
+    }
+    *pHead = newHead;
+}
+
+void CreateList(int* nums, int length, ListNode** pHead)
+{
+    if (nums == NULL || length <= 0)
+        return;
+
+    *pHead = NULL;
+    for (int i = length -1; i >= 0; i--)
+    {
+        ListNode* pNewNode = new ListNode;
+        pNewNode->m_nKey = nums[i];
+        pNewNode->m_pNext = *pHead;
+        *pHead = pNewNode;
+    }
+}
+
+void PrintList(ListNode* head)
+{
+    if (head != NULL)
+    {
+        ListNode* p = head;
+        while (p != NULL)
+        {
+            cout << p->m_nKey << endl;
+            p = p->m_pNext;
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int nums1[] = {1,2,3,4,5};
+    int len1 = 5;
+    ListNode* head1;
+    CreateList(nums1, 5, &head1);
+    PrintList(head1);
+    ReverseList(&head1);
+    PrintList(head1);
+
+    int nums2[] = {1};
+    int len2 = 1;
+    ListNode* head2;
+    CreateList(nums2, len2, &head2);
+    PrintList(head2);
+    ReverseList(&head2);
+    PrintList(head2);
+
+    int *nums3 = NULL;
+    int len3 = 0;
+    ListNode* head3 = NULL;
+    ReverseList(&head3);
+    PrintList(head3);
+    return 0;
+}
