9bf362dc47f729b20e001ed2952d173e59c6e1ab
Wang Junming <wangjmx@gmail.com>
2017-10-28 19:07:33 -0700

    draft

---
+++ 19_MirrorOfBinaryTree.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+struct BinaryTreeNode
+{
+    int m_nValue;
+    BinaryTreeNode* m_pLeft;
+    BinaryTreeNode* m_pRight;
+};
+
+void Mirror(BinaryTreeNode* root)
+{
+    if (root == NULL)
+        return;
+    BinaryTreeNode* temp = root->m_pLeft;
+    root->m_pLeft = root->m_pRight;
+    root->m_pRight = temp;
+    Mirror(root->m_pLeft);
+    Mirror(root->m_pRight);
+}
+
+void PrintBinaryTree(BinaryTreeNode* root)
+{
+    deque<BinaryTreeNode*> q1, q2;
+    q1.push_back(root);
+    do
+    {
+        do
+        {
+            BinaryTreeNode* u = q1.front();
+            q1.pop_front();
+            cout << u->m_nValue << " ";
+            if (u->m_pLeft)
+                q2.push_back(u->m_pLeft);
+            if (u->m_pRight)
+                q2.push_back(u->m_pRight);
+        }while (!q1.empty());
+        cout << endl;
+        q1.swap(q2);
+    }while (!q1.empty());
+}
+       
+int main()
+{
+    BinaryTreeNode* root1 = new BinaryTreeNode;
+    root1->m_nValue
+    return 0;
+}
