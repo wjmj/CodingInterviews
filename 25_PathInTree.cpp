dade97bda626becbf55d4ab2ef65d42f315af119
Wang Junming <wangjmx@gmail.com>
2017-11-06 14:34:58 +0800

    find paths that equal a given value

--- 25_PathInTree.cpp
+++ 25_PathInTree.cpp
@@ -10,19 +10,65 @@ struct BinaryTreeNode
     BinaryTreeNode* m_pRight;
 };
 
+void FindPathCore(BinaryTreeNode* pRoot, int value, vector<int> &path)
+{
+    if (pRoot->m_pLeft == NULL && pRoot->m_pRight == NULL)
+    {
+        if (value == pRoot->m_nValue)
+        {
+            path.push_back(pRoot->m_nValue);
+            for (int i = 0; i < path.size(); i++)
+            {
+                cout << path[i] << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+    int residual = value - pRoot->m_nValue;
+    path.push_back(pRoot->m_nValue);
+    if (pRoot->m_pLeft != NULL)FindPathCore(pRoot->m_pLeft, residual, path);
+    if (pRoot->m_pRight != NULL)FindPathCore(pRoot->m_pRight, residual, path);
+    path.pop_back();
+}
+
 void FindPath(BinaryTreeNode* pRoot, int value)
 {
-    if (pRoot == NULL || value <= 0)
+    if (pRoot == NULL)
         return;
 
-    vector<BinaryTreeNode*> path;
-    int residual;
-    path.insert(pRoot->m_nValue);
-    residual = value - pRoot->m_nValue;
-    while (!path.empty())
-    {
-        if 
+    vector<int> path;
+    FindPathCore(pRoot, value, path);
+}
 int main()
 {
+    BinaryTreeNode* root1 = new BinaryTreeNode;
+    root1->m_nValue = 8;
+    root1->m_pLeft = new BinaryTreeNode;
+    root1->m_pLeft->m_nValue = 6;
+    root1->m_pRight = new BinaryTreeNode;
+    root1->m_pRight->m_nValue = 10;
+    root1->m_pLeft->m_pLeft = new BinaryTreeNode;
+    root1->m_pLeft->m_pLeft->m_nValue = 5;
+    root1->m_pLeft->m_pLeft->m_pLeft = NULL;
+    root1->m_pLeft->m_pLeft->m_pRight = NULL;
+    root1->m_pLeft->m_pRight = new BinaryTreeNode;
+    root1->m_pLeft->m_pRight->m_nValue = 7;
+    root1->m_pLeft->m_pRight->m_pLeft = NULL;
+    root1->m_pLeft->m_pRight->m_pRight = NULL;
+    root1->m_pRight->m_pLeft = new BinaryTreeNode;
+    root1->m_pRight->m_pLeft->m_nValue = 9;
+    root1->m_pRight->m_pLeft->m_pLeft = NULL;
+    root1->m_pRight->m_pLeft->m_pRight = NULL;
+    root1->m_pRight->m_pRight = new BinaryTreeNode;
+    root1->m_pRight->m_pRight->m_nValue = 11;
+    root1->m_pRight->m_pRight->m_pLeft = NULL;
+    root1->m_pRight->m_pRight->m_pRight = NULL;
+
+    FindPath(root1, 19);
+    FindPath(root1, 21);
+    FindPath(root1, 29);
+    FindPath(root1, 30);
+
     return 0;
 }
